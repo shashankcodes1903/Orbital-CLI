c05ebb107062f7e7bf5f78552956b2b718c6f2b5
shashankcodes1903 <shashankkrtiwari007@gmail.com>
2026-03-16 00:27:51 +0530

    03_Authentication

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,30 @@
 import express from "express";
+import { toNodeHandler } from "better-auth/node";
 import dotenv from "dotenv"
-
+import cors from "cors";
+import { auth } from "./lib/auth.js";
 dotenv.config();
 
 const app=express();
+
+app.use(
+	cors({
+		origin: "http://localhost:3000", // Replace with your frontend's origin
+		methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+		credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+	})
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
+
+app.get("/api/me", async (req, res) => {
+	const session = await auth.api.getSession({
+		headers: fromNodeHeaders(req.headers),
+	});
+	return res.json(session);
+});
 
 app.get("/health" , (req, res)=>{
     res.send("OK")
